34b7f56e5f5807748813691739f463731d33853b
ludihan <65617704+ludihan@users.noreply.github.com>
2025-11-24 04:43:57 -0300

    feat: create menu and separate plugins

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 use std::{
-    collections::HashMap,
     f32::consts::{FRAC_PI_2, PI},
     ops::Range,
 };
 
 use bevy::{
     camera::ScalingMode,
-    ecs::system::SystemId,
     input::mouse::{AccumulatedMouseMotion, AccumulatedMouseScroll},
     prelude::*,
 };
-use serde::{Deserialize, Serialize};
+
+mod editor;
+mod game;
+mod menu;
+
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash, Default)]
+enum GameState {
+    #[default]
+    Menu,
+    Editor,
+    Level,
+    Settings,
+    CustomLevel,
+    Exit,
+}
+
+#[derive(Resource, Debug)]
+struct LevelIndex(usize);
+
+#[derive(Resource, Debug)]
+struct CustomLevel(std::path::PathBuf);
+
+#[derive(Resource, Debug)]
+struct Levels(Vec<Level>);
+
+#[derive(Debug)]
+struct Level {}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup, load_menu, load_level).chain())
-        .add_systems(Update, (orbit, switch_projection, zoom))
+        .add_plugins(game::GamePlugin)
+        .add_systems(Startup, setup)
         .run();
 }
+
+#[derive(Component)]
+struct IsProjectionOrtho(bool);
 
 #[derive(Debug, Resource)]
 struct CameraSettings {
@@ -64,52 +90,7 @@ impl Default for CameraSettings {
     }
 }
 
-#[derive(Resource)]
-struct GameData {
-    load_menu_id: SystemId,
-    load_level_id: SystemId,
-    load_editor_id: SystemId,
-    load_custom_level_id: SystemId,
-}
-
-#[derive(Serialize, Deserialize, Asset, TypePath)]
-struct LevelData {
-    data: HashMap<IVec3, ObjectType>,
-}
-
-#[derive(Serialize, Deserialize)]
-enum ObjectType {
-    Cube,
-    Ramp(Quat),
-}
-
-#[derive(Component)]
-struct IsProjectionOrtho(bool);
-
-fn load_menu(mut commands: Commands) {
-    /*
-    commands.spawn((
-        Name::new("Instructions"),
-        Text::new(
-            "Mouse up or down: pitch\n\
-            Mouse left or right: yaw\n\
-            Mouse buttons: roll",
-        ),
-        Node {
-            position_type: PositionType::Absolute,
-            top: px(12),
-            left: px(12),
-            ..default()
-        },
-    ));
-    */
-}
-
-fn load_level(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     commands.spawn((
         Name::new("CurrentProjection"),
         Text::new("Orthographic"),
@@ -142,151 +123,4 @@ fn load_level(
     ));
 
     commands.insert_resource(camera_settings);
-
-    commands.spawn((
-        Name::new("Plane"),
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0.3, 0.5, 0.3),
-            // Turning off culling keeps the plane visible when viewed from beneath.
-            cull_mode: None,
-            ..default()
-        })),
-    ));
-
-    commands.spawn((
-        Name::new("Cube"),
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
-        Transform::from_xyz(1.5, 0.51, 1.5),
-    ));
-
-    commands.spawn((
-        Name::new("Light"),
-        PointLight::default(),
-        Transform::from_xyz(3.0, 8.0, 5.0),
-    ));
-}
-fn load_editor(mut commands: Commands) {}
-fn load_custom_level(mut commands: Commands) {}
-
-/// Set up a simple 3D scene
-fn setup(mut commands: Commands) {
-    let game_data = GameData {
-        load_menu_id: commands.register_system(load_menu),
-        load_level_id: commands.register_system(load_level),
-        load_editor_id: commands.register_system(load_editor),
-        load_custom_level_id: commands.register_system(load_custom_level),
-    };
-
-    commands.insert_resource(game_data);
-}
-
-fn switch_projection(
-    mut camera: Single<&mut Projection, With<Camera>>,
-    current_projection: Single<(&mut IsProjectionOrtho, &mut Text)>,
-    camera_settings: Res<CameraSettings>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        let (mut is_ortho, mut text_projection) = current_projection.into_inner();
-        if is_ortho.0 {
-            *text_projection = "Perspective".into();
-        } else {
-            *text_projection = "Orthographic".into();
-        }
-
-        (*is_ortho).0 = !(*is_ortho).0;
-        // Switch projection type
-        **camera = match **camera {
-            Projection::Orthographic(_) => Projection::Perspective(PerspectiveProjection {
-                fov: camera_settings.perspective_zoom_range.start,
-                ..default()
-            }),
-            Projection::Perspective(_) => Projection::Orthographic(OrthographicProjection {
-                scaling_mode: ScalingMode::FixedVertical {
-                    viewport_height: camera_settings.orthographic_viewport_height,
-                },
-                ..OrthographicProjection::default_3d()
-            }),
-            _ => return,
-        }
-    }
-}
-
-fn zoom(
-    camera: Single<&mut Projection, With<Camera>>,
-    camera_settings: Res<CameraSettings>,
-    mouse_wheel_input: Res<AccumulatedMouseScroll>,
-) {
-    // Usually, you won't need to handle both types of projection,
-    // but doing so makes for a more complete example.
-    match *camera.into_inner() {
-        Projection::Orthographic(ref mut orthographic) => {
-            // We want scrolling up to zoom in, decreasing the scale, so we negate the delta.
-            let delta_zoom = -mouse_wheel_input.delta.y * camera_settings.orthographic_zoom_speed;
-            // When changing scales, logarithmic changes are more intuitive.
-            // To get this effect, we add 1 to the delta, so that a delta of 0
-            // results in no multiplicative effect, positive values result in a multiplicative increase,
-            // and negative values result in multiplicative decreases.
-            let multiplicative_zoom = 1. + delta_zoom;
-
-            orthographic.scale = (orthographic.scale * multiplicative_zoom).clamp(
-                camera_settings.orthographic_zoom_range.start,
-                camera_settings.orthographic_zoom_range.end,
-            );
-        }
-        Projection::Perspective(ref mut perspective) => {
-            // We want scrolling up to zoom in, decreasing the scale, so we negate the delta.
-            let delta_zoom = -mouse_wheel_input.delta.y * camera_settings.perspective_zoom_speed;
-
-            // Adjust the field of view, but keep it within our stated range.
-            perspective.fov = (perspective.fov + delta_zoom).clamp(
-                camera_settings.perspective_zoom_range.start,
-                camera_settings.perspective_zoom_range.end,
-            );
-        }
-        _ => (),
-    }
-}
-
-fn orbit(
-    mut camera: Single<&mut Transform, With<Camera>>,
-    camera_settings: Res<CameraSettings>,
-    mouse_buttons: Res<ButtonInput<MouseButton>>,
-    mouse_motion: Res<AccumulatedMouseMotion>,
-    time: Res<Time>,
-) {
-    let target = Vec3::ZERO;
-    let delta = mouse_motion.delta;
-    let mut delta_roll = 0.0;
-
-    // Mouse motion is one of the few inputs that should not be multiplied by delta time,
-    // as we are already receiving the full movement since the last frame was rendered. Multiplying
-    // by delta time here would make the movement slower that it should be.
-    let delta_pitch = -(delta.y * camera_settings.pitch_speed);
-    let delta_yaw = -(delta.x * camera_settings.yaw_speed);
-
-    // Conversely, we DO need to factor in delta time for mouse button inputs.
-    delta_roll *= camera_settings.roll_speed * time.delta_secs();
-
-    // Obtain the existing pitch, yaw, and roll values from the transform.
-    let (yaw, pitch, roll) = camera.rotation.to_euler(EulerRot::YXZ);
-
-    // Establish the new yaw and pitch, preventing the pitch value from exceeding our limits.
-    let pitch = (pitch + delta_pitch).clamp(
-        camera_settings.pitch_range.start,
-        camera_settings.pitch_range.end,
-    );
-    let roll = roll + delta_roll;
-    let yaw = yaw + delta_yaw;
-
-    // Only orbit if pressing left mouse button
-    if mouse_buttons.pressed(MouseButton::Right) {
-        camera.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-    };
-
-    // Adjust the translation to maintain the correct orientation toward the orbit target.
-    // In our example it's a static target, but this could easily be customized.
-    camera.translation = target - camera.forward() * camera_settings.orbit_distance;
 }
---
+++ src/menu.rs
@@ -0,0 +1,161 @@
+use bevy::{color::palettes::basic::*, input_focus::InputFocus, prelude::*};
+
+use crate::GameState;
+
+pub struct MenuPlugin;
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.45, 0.45, 0.45);
+
+#[derive(Component)]
+struct ButtonType(GameState);
+
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Menu), menu_setup)
+            .add_systems(OnEnter(GameState::Editor), editor_setup)
+            .add_systems(OnEnter(GameState::Settings), settings_setup)
+            .add_systems(OnEnter(GameState::CustomLevel), custom_level_setup)
+            .add_systems(OnEnter(GameState::Exit), exit_setup)
+            .add_systems(Update, menu_system)
+            .init_resource::<InputFocus>();
+    }
+}
+
+fn menu_setup(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.spawn((
+        DespawnOnExit(GameState::Menu),
+        Node {
+            height: percent(100),
+            width: percent(100),
+            justify_content: JustifyContent::SpaceBetween,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        children![
+            (
+                Node {
+                    height: percent(100),
+                    width: percent(100),
+                    margin: UiRect::all(px(10f32)),
+                    ..default()
+                },
+                children![(
+                    Text::new("Isomagic"),
+                    TextFont {
+                        font: assets.load("fonts/Iosevka.ttc"),
+                        font_size: 100.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                    TextShadow::default(),
+                )]
+            ),
+            (
+                Node {
+                    height: percent(100),
+                    width: percent(100),
+                    align_items: AlignItems::FlexStart,
+                    justify_content: JustifyContent::FlexEnd,
+                    flex_direction: FlexDirection::Column,
+                    margin: UiRect::all(px(10f32)),
+                    ..default()
+                },
+                children![
+                    button(GameState::Level, "Levels", &assets),
+                    button(GameState::CustomLevel, "Custom Levels", &assets),
+                    button(GameState::Editor, "Editor", &assets),
+                    button(GameState::Settings, "Settings", &assets),
+                    button(GameState::Exit, "Exit", &assets),
+                ],
+            )
+        ],
+    ));
+}
+
+fn button(game_state: GameState, text: impl Into<String>, assets: &AssetServer) -> impl Bundle {
+    ((
+        ButtonType(game_state),
+        Button,
+        Node {
+            height: px(65),
+            border: UiRect::all(px(5)),
+            // horizontally center child text
+            justify_content: JustifyContent::Center,
+            // vertically center child text
+            align_items: AlignItems::Center,
+            padding: UiRect::all(px(5f32)),
+            ..default()
+        },
+        BorderColor::all(Color::WHITE),
+        BorderRadius::MAX,
+        BackgroundColor(Color::BLACK),
+        children![(
+            Text::new(text),
+            TextFont {
+                font: assets.load("fonts/Iosevka.ttc"),
+                font_size: 33.0,
+                ..default()
+            },
+            TextColor(Color::srgb(0.9, 0.9, 0.9)),
+            TextShadow::default(),
+        )],
+    ),)
+}
+
+fn menu_system(
+    mut input_focus: ResMut<InputFocus>,
+    mut interaction_query: Query<
+        (
+            Entity,
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &mut Button,
+            &Children,
+            &ButtonType,
+        ),
+        Changed<Interaction>,
+    >,
+    mut next_state: ResMut<NextState<GameState>>,
+    mut _text_query: Query<&mut Text>,
+) {
+    for (entity, interaction, mut color, mut border_color, mut button, _children, button_type) in
+        &mut interaction_query
+    {
+        // let mut text = text_query.get_mut(children[0]).unwrap();
+
+        match *interaction {
+            Interaction::Pressed => {
+                input_focus.set(entity);
+                //**text = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                *border_color = BorderColor::all(Color::WHITE);
+
+                // The accessibility system's only update the button's state when the `Button` component is marked as changed.
+                button.set_changed();
+                next_state.set(button_type.0.clone());
+            }
+            Interaction::Hovered => {
+                input_focus.set(entity);
+                //**text = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                *border_color = BorderColor::all(Color::WHITE);
+                button.set_changed();
+            }
+            Interaction::None => {
+                input_focus.clear();
+                //**text = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                *border_color = BorderColor::all(Color::BLACK);
+            }
+        }
+    }
+}
+
+fn editor_setup() {}
+fn level_setup(commands: Commands) {}
+fn settings_setup() {}
+fn custom_level_setup() {}
+fn exit_setup() {}
